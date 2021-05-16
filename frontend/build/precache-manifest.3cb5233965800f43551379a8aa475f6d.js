3145f856b87942a83613e1f094536aa7d60b68e5
Ferry <alessandro.ferrari.2004@gmail.com>
2021-05-16 11:38:51 -0400

    first commit

---
+++ frontend/build/precache-manifest.3cb5233965800f43551379a8aa475f6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed715ef78c031395ba8b4548fb32f6a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b843e1f726ad4e6c8d5",
+    "url": "/static/css/main.e5549477.chunk.css"
+  },
+  {
+    "revision": "8b9d18f4af8461fe9403",
+    "url": "/static/js/2.919cc4e8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.919cc4e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b843e1f726ad4e6c8d5",
+    "url": "/static/js/main.ac3d124d.chunk.js"
+  },
+  {
+    "revision": "4792b66c7ea268d3b997",
+    "url": "/static/js/runtime-main.d43be887.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
